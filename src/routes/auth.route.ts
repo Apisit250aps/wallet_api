71f372236ce844d12ffed14674cac3b0441c322d
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-07-26 20:04:37 +0700

    remove test auth

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,15 +1,9 @@
 import { Router } from "express"
 import authController from "../controllers/auth.controller"
-import { authenticateJWT } from "../middlewares/auth.middleware"
-import { Request, Response } from "express"
+
 const auth = Router()
 
 auth.post("/register", authController.userRegister)
 auth.post("/login", authController.userLogin)
 
-auth.get("/test",authenticateJWT, (req: Request, res: Response) => {
-    console.log(req.body)
-  res.status(200)
-  res.json({message:"hello"})
-})
 export default auth
