55ce13c78b47aa3c04c839af899f452ed44b83dc
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-07-26 10:52:54 +0700

    wallet_dev_connected

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/expenseTracker")
+    await mongoose.connect("mongodb://localhost:27017/wallet_dev")
     console.log("MongoDB connected")
   } catch (error) {
     console.error(error)
