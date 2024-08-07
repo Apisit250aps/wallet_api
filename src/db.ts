a76bd662e60509d00175014a662721200dace6a0
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-07 20:48:35 +0700

    memory, time out

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose"
 
-
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://apisit250aps:Phgs5srZYBulqDab@cluster0.jwfno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect(
+      "mongodb+srv://apisit250aps:Phgs5srZYBulqDab@cluster0.jwfno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
 
     console.log("MongoDB connected")
   } catch (error) {
