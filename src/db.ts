1a6c8d441688cbc66c385d1595624315b2ac345b
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-07 22:46:45 +0700

    db

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose"
-mongoose.set('debug', true);
+mongoose.set("debug", true)
 const connectDB = async () => {
   try {
     await mongoose.connect(
       "mongodb+srv://apisit250aps:Phgs5srZYBulqDab@cluster0.jwfno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
       {
-        serverSelectionTimeoutMS: 30000, 
+        serverSelectionTimeoutMS: 30000,
+        socketTimeoutMS: 45000, // เพิ่มเวลา socket timeout
+        connectTimeoutMS: 30000 // เพิ่มเวลา connect timeout
       }
     )
 
