f8e99a50b82ff712e6574669f57b37bb946c7855
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-07 22:41:05 +0700

    db duration

--- src/db.ts
+++ src/db.ts
@@ -4,9 +4,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(
       "mongodb+srv://apisit250aps:Phgs5srZYBulqDab@cluster0.jwfno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      // {
-      //   serverSelectionTimeoutMS: 60000 // Increase timeout (in milliseconds)
-      // }
+      {
+        serverSelectionTimeoutMS: 30000, 
+      }
     )
 
     console.log("MongoDB connected")
