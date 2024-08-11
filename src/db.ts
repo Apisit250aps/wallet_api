6662fff8ba0f247e1b7a520e1d69364a26142427
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-11 17:39:14 +0700

    dbconnected

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://apisit250aps:jUS_XYN3Eg#va!f@cluster0.jwfno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect("mongodb+srv://apisit250aps:Phgs5srZYBulqDab@cluster0.jwfno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
     console.log("MongoDB connected")
   } catch (error) {
