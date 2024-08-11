d7359f35fa35ce0f5267b5481de600f6978b2263
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-08-11 17:43:51 +0700

    build

--- dist/db.js
+++ dist/db.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const connectDB = async () => {
     try {
-        await mongoose_1.default.connect("mongodb://localhost:27017/wallet_dev");
+        await mongoose_1.default.connect("mongodb+srv://apisit250aps:Phgs5srZYBulqDab@cluster0.jwfno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("MongoDB connected");
     }
     catch (error) {
